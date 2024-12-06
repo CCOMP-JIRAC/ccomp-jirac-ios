1b629a4fcbcee474342158eb31ab5d9ee9483029
vvtkfkddl11 <vvtkfkddl11@naver.com>
2024-12-06 17:32:36 +0900

    Initial Commit

---
+++ ccomp-jirac-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ccomp-jirac-ios
+//
+//  Created by 김사랑 on 12/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
